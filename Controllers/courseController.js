882bbfa3bb6d0ba2449f5b9bb41e03cabe2310f0
JimRef <jimmyrefugio11@gmail.com>
2023-02-06 11:06:27 +0800

    Add activity code - S40

--- Controllers/courseController.js
+++ Controllers/courseController.js
@@ -36,3 +36,125 @@ module.exports.addCourse = (request, response) =>{
 	}
 	
 }
+
+// Create a controller wherein it will retrieved all the courses (active/inactive courses)
+
+module.exports.allCourses = (request, response) => {
+	const userData = auth.decode(request.headers.authorization);
+	console.log(userData)
+
+	if (!userData.isAdmin) {
+		return response.send("You don't have access to this route!")
+	} else {
+		Course.find({})
+		.then(result => {
+			return response.send(result);
+		})
+		.catch(error => {
+			return response.send(error)
+		})
+	}
+}
+
+// Create a crontroller wherein it will retrieve courses that are active.
+
+module.exports.allActiveCourses = (request, response) =>{
+
+	Course.find({isActive: true})
+	.then(result => response.send(result))
+	.catch(error => response.send(error))
+}
+
+/*
+	
+*/
+
+module.exports.allInactiveCourses = (request, response) =>{
+	const userData = auth.decode(request.headers.authorization);
+
+	if (!userData.isAdmin) {
+		return response.send("You don't have access to this route!")
+	} else {
+		Course.find({isActive: false})
+		.then(result => response.send(result))
+		.catch(error => response.send(error))
+	}
+}
+
+// This controller will get details of specific course
+
+module.exports.courseDetails = (request, response) =>{
+	const courseId = request.params.courseId;
+
+	Course.findById(courseId)
+	.then(result => response.send(result))
+	.catch(error => response.send(error));
+}
+
+// This controller is for updating specific course
+/*
+	Business logic:
+		1. We are going to edit/update the course
+		that is in the params.
+*/
+
+module.exports.updateCourse = async (request,response) => {
+
+	const userData = auth.decode(request.headers.authorization);
+
+	const courseId = request.params.courseId;
+
+	const input = request.body;
+
+	if (!userData.isAdmin) {
+		return response.send("You don't have access to this page!")
+	} else {
+		await Course.findOne({_id: courseId})
+		.then(result => {
+			if (result === null) {
+				return response.send("Course Id not found!");
+			} else {
+				let updatedCourse = {
+							name: input.name,
+							description: input.description,
+							price: input.price
+						}
+
+						Course.findByIdAndUpdate(courseId, updatedCourse, {new: true})
+						.then(result => {
+							console.log(result)
+							return response.send(result)
+						}) 
+						.catch(error => response.send(error))
+			}
+		})
+		.catch(error => response.send(error))
+	}
+}
+
+// Controller for archiving
+
+module.exports.archiveCourse = (request, response) => {
+	const userData = auth.decode(request.headers.authorization);
+	const courseId = request.params.courseId;
+	const input = request.body
+
+	if (!userData.isAdmin) {
+		return response.send("You don't have accessto this page!")
+	} else {
+		Course.findById(courseId)
+		.then(result => {
+			if (result === null) {
+				return response.send("Course Id not found!")
+			} else {
+				let archive = {
+					isActive: input.isActive
+				}
+				Course.findByIdAndUpdate(courseId, archive, {new:true})
+				.then(result => response.send(result))
+				.catch(error => response.send(error))
+			}
+		})
+		.catch(error => response.send(error))
+	}
+}
--- Routes/courseRoutes.js
+++ Routes/courseRoutes.js
@@ -3,9 +3,26 @@ const router = express.Router();
 const courseController = require("../Controllers/courseController");
 const auth = require("../auth.js");
 
+// [ROUTE WITHOUT PARAMS]
+
 // Route for creating a course
 router.post("/", auth.verify,courseController.addCourse);
+// Route for retrieving all courses
+router.get("/all", auth.verify, courseController.allCourses);
+// Route for retrieving all active courses
+router.get("/allActive", courseController.allActiveCourses);
+// router for retrieving  inactive courses
+router.get("/allInactive", auth.verify, courseController.allInactiveCourses)
 
+
+// [ROUTE WITH PARAMS]
+
+// Route for retrieving details of specific course
+router.get("/:courseId", courseController.courseDetails);
+
+router.put("/update/:courseId", auth.verify, courseController.updateCourse)
+
+router.patch("/archive/:courseId", auth.verify, courseController.archiveCourse)
 
 
 module.exports = router;
